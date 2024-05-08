2add071106df222561ba80e15a5ba7d7657ed1c4
Pauljesmarc <pjboyflores@gmail.com>
2024-05-08 20:27:49 +0800

    added hint button to modal dialog

---
+++ src/main/java/org/example/demo/Player.java
@@ -0,0 +1,10 @@
+package org.example.demo;
+
+import javafx.scene.paint.Color;
+
+public class Player extends Entity{
+    public Player(int x, int y, int w, int h, Color color) {
+        super(x, y, w, h, color);
+    }
+
+}
---
+++ src/main/java/org/example/demo/Question.java
@@ -0,0 +1,19 @@
+package org.example.demo;
+
+public class Question   {
+
+    public String[][] getQuestions(){
+        return new String[][]{
+                {"Solve 4x−7(2−x) = 3 x + 2", "2" ,"next to one"},
+                {"Solve 1/x = 1/5", "5" ,"Really?"},
+                {"Solve for z: z - 5 = 6", "11","Try Premium for $5/month"},
+                {"Solve for x: 3x – 5 = 10","5","Try Premium for $5/month"},
+                {"Solve for m: 2(m + 6) = 48","18","Try Premium for $5/month"},
+                {"Solve for y: 13(y–4)–3(y–9)–5(y+4)=0","9","Try Premium for $5/month"},
+                {"If (a−8)/3=(a−3)/2, then a = ?","-7","Try Premium for $5/month"},
+                {"Solve for x: (x+2)(x+3)+(x–3)(x–2)–2x(x+1)=0","6","Try Premium for $5/month"},
+                {"Everyone's favorite number.","69","Your not Man enough"},
+                {"Bonus","","Try Premium for $5/month"},
+        };
+    }
+}
